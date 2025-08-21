2d9b11039a7cdac67c5baa42d2ce830ac7a920fd
SrijaniBhattacherjee <66897981+SrijaniBhattacherjee@users.noreply.github.com>
2025-08-22 02:28:43 +0530

    Finding even numbers in array

---
+++ EvenNumbersinAnArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace ArraysPractice
+{
+	public class MaxElementinArray
+	{
+		public static void Main(string[] args)
+		{
+            //size of an array taken
+            Console.WriteLine("Enter the size of an array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            //initialized the array
+            int[] numbers = new int[size];
+
+            //taking the elements of an array
+            Console.WriteLine("Enter elements of the array :");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (numbers.Length > 0)
+            {
+                int count = 0;
+                Console.WriteLine("Enter the size of an array:");
+                int size2 = Convert.ToInt32(Console.ReadLine());
+
+                int[] arr = new int[size2];
+
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        count += 1;
+                    }
+                }
+                Console.WriteLine($"The total count of even numbers in the array : {count}");
+            }
+            else
+            {
+                Console.WriteLine("The array is empty. please provide a value.");
+            }
+        }
+	}
+}
+
