19c59ee3f1d152eb5811f71d16f02ee6b372072c
SrijaniBhattacherjee <66897981+SrijaniBhattacherjee@users.noreply.github.com>
2025-08-27 17:22:18 +0530

    Find Duplicate elements

---
+++ DuplicateElementsInArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace ArraysPractice
+{
+	public class DuplicateElementsInArray
+	{
+		public static void Main(string[] args)
+		{
+            Console.WriteLine("Enter the size of an array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            //initialized the array
+            int[] numbers = new int[size];
+
+            //taking the elements of an array
+            Console.WriteLine("Enter elements of the array :");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] newArr = new int[size];
+            int m = 0;
+
+            // checking for the duplicates
+            if (numbers.Length > 0)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    for (int j = i + 1; j < numbers.Length; j++)
+                    {
+                        if (numbers[i] == numbers[j])
+                        {
+                            newArr[m++] = numbers[i];
+                        }
+                    }
+                }
+            }
+
+            //checking if the new array has any element
+            if (newArr.All(n => n == 0))
+            {
+
+                Console.WriteLine("All the elemnts are unique");
+            }
+            else
+            {
+                Console.WriteLine("Duplicate Array Elements:");
+                foreach (int num in newArr)
+                {
+                    if (num == 0) continue; 
+                    {
+                        Console.Write(num + " ");
+                    }
+                }
+            }
+        }
+    }
+}
+
