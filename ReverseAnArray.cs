8fef8f43cd35a55357d152258e178442c2165078
SrijaniBhattacherjee <66897981+SrijaniBhattacherjee@users.noreply.github.com>
2025-09-03 01:20:45 +0530

    Commented out code

--- ReverseAnArray.cs
+++ ReverseAnArray.cs
@@ -41,23 +41,26 @@ namespace ArraysPractice
 
         // without creating a new array reverses
 
-        public class ReverseArrayForLoop
-        {
-                int[] numbers = { 1, 2, 3, 4, 5 };
+        //public class ReverseArrayForLoop
+        //{
+        //    public static void Main(string[] args)
+        //    {
+        //        int[] numbers = { 1, 2, 3, 4, 5 };
 
-                for (int i = 0; i < numbers.Length / 2; i++)
-                {
-                    int temp = numbers[i];
-                    numbers[i] = numbers[numbers.Length - 1 - i];
-                    numbers[numbers.Length - 1 - i] = temp;
-                }
+        //        for (int i = 0; i < numbers.Length / 2; i++)
+        //        {
+        //            int temp = numbers[i];
+        //            numbers[i] = numbers[numbers.Length - 1 - i];
+        //            numbers[numbers.Length - 1 - i] = temp;
+        //        }
 
-                Console.WriteLine("The reversed array is:");
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    Console.Write(numbers[i] + ", ");
-                }
-        }
+        //        Console.WriteLine("The reversed array is:");
+        //        for (int i = 0; i < numbers.Length; i++)
+        //        {
+        //            Console.Write(numbers[i] + ", ");
+        //        }
+        //    }
+        //}
 
     }
 
