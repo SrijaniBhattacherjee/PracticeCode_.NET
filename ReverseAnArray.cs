c2da8f9fc7c2ed6c54f994927723f12b412b397c
SrijaniBhattacherjee <66897981+SrijaniBhattacherjee@users.noreply.github.com>
2025-09-03 01:15:57 +0530

    Added sorting and reversing an array

---
+++ ReverseAnArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace ArraysPractice
+{
+    public class ReverseAnArray
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of an array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            //initialized the array
+            int[] numbers = new int[size];
+
+            //taking the elements of an array
+            Console.WriteLine("Enter elements of the array :");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // arr 1 = {1,2,3,4,5} ; new arr = {5,4,3,2,1}
+
+            int[] newNumbers = new int[size];
+
+
+            int j = 0;
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                newNumbers[j++] = numbers[i];
+            }
+
+            Console.WriteLine("The reverse array is : ");
+            foreach (int num in newNumbers)
+            {
+                Console.Write(num + ",");
+
+            }
+
+        }
+
+        // without creating a new array reverses
+
+        public class ReverseArrayForLoop
+        {
+            public static void Main(string[] args)
+            {
+                int[] numbers = { 1, 2, 3, 4, 5 };
+
+                for (int i = 0; i < numbers.Length / 2; i++)
+                {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[numbers.Length - 1 - i];
+                    numbers[numbers.Length - 1 - i] = temp;
+                }
+
+                Console.WriteLine("The reversed array is:");
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.Write(numbers[i] + ", ");
+                }
+            }
+        }
+
+    }
+
+
+}
+
---
+++ SortingAnArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ArraysPractice
+{
+	public class SortingAnArray
+	{
+
+        // Bubble sort
+		public static void Main(string[] args)
+		{
+            int[] numbers = { 5, 2, 8, 1, 3 };
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        // swap
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorted Array:");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+        }
+	}
+}
+
